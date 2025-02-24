762cbc8762d25a9bc3f8adbc4ceb8bdf7050fb4c
daniel <daniel.romero@inria.fr>
2025-02-24 11:29:36 +0100

    feat: Add bash script for building with qmake

---
+++ qmake/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROJECT_DIR=$(realpath "../")
+BUILD_DIR="$PROJECT_DIR/build"
+
+rm -rf "$BUILD_DIR"
+mkdir -p "$BUILD_DIR"
+cd "$BUILD_DIR" || exit 1
+
+qmake ../venice-qt.pro
+make 
